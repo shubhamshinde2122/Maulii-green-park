ceb39c2d29bb3b426a9251914827473f6e63b104
SHUBHAM <shubhamps2122@gmail.com>
2026-01-07 12:22:24 +0530

    feat: activated web3forms with access key

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 // Web3Forms Access Key
 // Get your key from https://web3forms.com/ and paste it here.
-export const WEB3FORMS_ACCESS_KEY = "YOUR_ACCESS_KEY_HERE";
+export const WEB3FORMS_ACCESS_KEY = "830ce0e9-d22a-4b08-9ba5-3e7c5413d1e1";
